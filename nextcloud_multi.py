52cbe78b75050f001c23fa631ae2301a0c4e0b96
nico <nico@magicbroccoli.de>
2018-11-13 02:13:51 +0100

    fix Windows filemode messup
    * set +x to all scripts ( damn windows )
    + added nextcloud num_updates_available warning to 1

--- nextcloud_multi.py
+++ nextcloud_multi.py
@@ -95,7 +95,8 @@ class NextcloudMultiGraph:
 				'graph_category nextcloud',
 				'num_updates_available.label available app updates',
 				'num_updates_available.info number of available app updates',
-				'num_updates_available.min 0'
+				'num_updates_available.min 0',
+				'num_updates_available.warning 1'
 			]
 		}
 
